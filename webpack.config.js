9fd6d03209296a422b12efbaf8cc7437378fb8f5
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2020-11-21 10:44:34 +0100

    Updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
-module.exports = function() {
+module.exports = function () {
   return {
     // prevent webpack using eval-sourcemap
     devtool: 'source-map',
     entry: {
       main: './src/index.js',
       options: './src/options.js',
+    },
+    /* these entries are needed to teach webpack we are not interested in any node libraries */
+    /* present in our source code, as we are only building for the web */
+    resolve: {
+      fallback: {
+        crypto: false,
+        path: false,
+      }
     }
   }
 }
